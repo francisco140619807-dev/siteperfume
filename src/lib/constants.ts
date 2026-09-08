12abe50877a8449a97eee4664d480ddac1967ee5
francisco140619807-dev <francisco140619807@gmail.com>
2026-09-08 20:16:48 -0300

    Add project constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const BRANDS = [
+  {src:"https://economizandocomtio.com.br/wp-content/uploads/2025/11/marcas-02-1.png",alt:"Marca 02"},
+  {src:"https://economizandocomtio.com.br/wp-content/uploads/2025/11/marcas-03-1.png",alt:"Marca 03"},
+  {src:"https://economizandocomtio.com.br/wp-content/uploads/2025/11/marcas-04-1.png",alt:"Marca 04"},
+  {src:"https://economizandocomtio.com.br/wp-content/uploads/2025/11/marcas-05-1.png",alt:"Marca 05"},
+  {src:"https://economizandocomtio.com.br/wp-content/uploads/2025/11/marcas-06-1.png",alt:"Marca 06"},
+  {src:"https://economizandocomtio.com.br/wp-content/uploads/2025/11/marcas-07-1.png",alt:"Marca 07"},
+  {src:"https://economizandocomtio.com.br/wp-content/uploads/2025/11/marcas-08-1.png",alt:"Marca 08"},
+  {src:"https://economizandocomtio.com.br/wp-content/uploads/2025/11/beleza.png",alt:"Beleza na Web"},
+  {src:"https://economizandocomtio.com.br/wp-content/uploads/2025/11/boticario.png",alt:"O Boticário"},
+];
+export const BENEFITS = ["Ofertas imperdíveis o dia todo","Cupons exclusivos","Produtos de qualidade","Perfumes","Diversas marcas","e muito mais"];
