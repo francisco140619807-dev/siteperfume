0eb49d9c1a02e8a60fd435b332ea495088726732
francisco140619807-dev <francisco140619807@gmail.com>
2026-09-08 20:16:36 -0300

    Add React entry point

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import { StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = document.getElementById("root");
+if (root) hydrateRoot(root, <StrictMode><App /></StrictMode>);
